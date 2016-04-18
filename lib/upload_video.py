a9caf09b79f8aedcb57a603536933e6d9a1cd032
Don Burks <don@donburks.com>
2016-04-18 15:19:53 -0700

    Changing retries and HttpErrors

--- lib/upload_video.py
+++ lib/upload_video.py
@@ -106,18 +106,9 @@ def initialize_download(options):
 
     if error is not None:
       print error
-      retry += 1
-      if retry > MAX_RETRIES:
-        exit("No longer attempting to retry.")
-
-      max_sleep = 2 ** retry
-      sleep_seconds = random.random() * max_sleep
-      print "Sleeping %f seconds and then retrying..." % sleep_seconds
-      time.sleep(sleep_seconds)
 
   options.file = options.tmp
   initialize_upload(options)	
-
 
 
 def initialize_upload(options):
